e2fcbef49c1b4b354895b1598f4a653e3380cbf0
dalwerajesh31 <dalwerajesh31@gmail.com>
2024-01-16 13:04:48 +0530

    Pattern_Prog_Push

---
+++ PtternPrograms/Project3/Project3/ClassWork.c
@@ -0,0 +1,66 @@
+//#include<stdio.h>
+//
+//
+//int main() {
+//
+//	
+//
+//		// Code for fucntion ==> strcmp
+//
+//	/*char str1[40], str2[40];
+//	puts("Enter First String : ");
+//	gets_s(str1,39);
+//	puts("Enter Second String : ");
+//	gets_s(str2, 39);
+//	int i = 0, flag = 0;
+//	while (str1[i] != '\0' || str2[i] != '\0') {
+//
+//		if(str1[i] == str2[i])
+//			i++;
+//		else if(str1[i] < str2[i])
+//		{
+//			flag = -1;
+//			break;
+//		}
+//		else
+//		{
+//			flag = 1;
+//			break;
+//		}
+//	}
+//	printf("%d", flag);*/
+//
+//	
+////=====================================================================
+//
+//		// Code for function ==> strcpy
+//
+//	/*char str1[40], str2[40];
+//	puts("Enter First String : ");
+//	gets_s(str1, 39);
+//	puts("Enter Second String : ");
+//	gets_s(str2, 39);
+//
+//	puts("Coping Second string into First string..... ");
+//	int i = 0;
+//	while (str2[i] != '\0') {
+//
+//		str1[i] = str2[i];
+//
+//		i++;
+//	}
+//	str1[i] = '\0';
+//
+//	puts(str1);
+//	puts(str2);*/
+//
+//
+////====================================================================
+//
+//	 //global variable
+//	
+//	
+//
+//}
+//
+//
---
+++ PtternPrograms/Project3/Project3/Recursion.c
@@ -0,0 +1,69 @@
+//#include<stdio.h>
+//
+//
+//void main()
+//{
+//	void rc();
+//	rc();
+//}
+//void rc()
+//{
+//	char ch;
+//	puts("Enter Character : ");
+//	ch = getchar();
+//	if (ch == '\n')
+//	{
+//		return;
+//	}
+//	else
+//	{
+//		rc();
+//	}
+//	putchar(ch);
+//}
+//
+//
+//
+//
+////int main() {
+////
+////	void getCharacter();
+////	getCharacter();
+////	
+////}
+////
+////	void fibonacci() {
+////		 /*void fibonacci();
+////	puts("Enter the number to print Fibonacci upto that no. : ");
+////	printf("%d %d ", 0, 1);
+////	fibonacci();*/
+////
+////		static a = 0, b = 1, c=0;
+////		if (c > 30) {
+////			return;
+////		}
+////		else
+////		{
+////			c = a + b;
+////			printf("%d ", c);
+////			a = b;
+////			b = c;
+////			fibonacci();
+////		}
+////
+////	}
+////
+////	void getCharacter() {
+////		char ch;
+////		puts("Enter the charcter : ");
+////		ch = getchar();
+////		if (ch == '\n') {
+////			return;
+////		}
+////		else
+////		{
+////			getCharacter();
+////		}
+////		putchar(ch);
+////
+////	}
